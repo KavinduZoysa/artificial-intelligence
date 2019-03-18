af4acc9dbda386478d460f9622474a8388610583
kavindu <kavindu@wso2.com>
2019-03-19 00:07:54 +0530

    Restore the saved session and print the parameters

--- samples/save_restore_model.py
+++ samples/save_restore_model.py
@@ -1,8 +1,26 @@
 import tensorflow as tf
 
-w1 = tf.Variable(tf.random_normal(shape=[2]), name='w1')
-w2 = tf.Variable(tf.random_normal(shape=[5]), name='w2')
-saver = tf.train.Saver()
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-saver.save(sess, 'models/my_test_models')
+
+# Save the model
+def save_model():
+    w1 = tf.Variable(tf.random_normal(shape=[2]), name='w1')
+    w2 = tf.Variable(tf.random_normal(shape=[5]), name='w2')
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess = tf.Session()
+        sess.run(tf.global_variables_initializer())
+        print(sess.run(w2))
+        saver.save(sess, 'models/my_test_models')
+
+
+# Restore the model
+def restore_model():
+    saver = tf.train.import_meta_graph('/home/kavindu/workspace/PycharmProjects/ai/samples/models/my_test_models.meta')
+    with tf.Session() as sess:
+        sess = tf.Session()
+        saver.restore(sess, tf.train.latest_checkpoint('/home/kavindu/workspace/PycharmProjects/ai/samples/models'))
+        print(sess.run('w2:0'))
+
+
+save_model()
+restore_model()
